17307432e8156a43477ee59f79a5b1eecf0066e1
Frykers <tonyh.wgtn@gmail.com>
2020-04-20 20:20:39 +1200

    Added table output and menu functionallity
    -Output function broken up into output() and percentage()
    -Menu added
    -Now outputs in a table format

--- two_dice_simulator.py
+++ two_dice_simulator.py
@@ -1,7 +1,7 @@
 # two_dice_simulator.py
 # Simulates 2 dice being rolled and outputs probablillity
 # By Tony Huang
-# Version 0.2
+# Version 1
 import random
 
 
@@ -10,7 +10,25 @@ def roll():
     return (random.randint(1, 6) + random.randint(1, 6)) # Simulates and adds up two d6 rolls
 
 
-def main():
+def percentage(totals):
+    """ Convert each total into percentages of all rolls"""
+    percentages = {}
+    for i in range(2, 13):
+        percentages[i] = (float(totals[i])/10)
+    return percentages
+
+
+def output(simulation_percentages):
+    """Outputs the percentages and expected percentages"""
+    EXPECTED_PERCENTAGES = {2: 2.78, 3: 5.56, 4: 8.33, 5: 11.11, 6: 13.89, 7: 16.67, 8: 13.89, 9: 11.11, 10: 8.33, 11: 5.56, 12: 2.78}
+
+    #Output results in table format
+    print("Total | Expected Value |Actual Value")
+    for i in range (2, 13):
+        print("{}     |{}             |{}   ".format(i,  EXPECTED_PERCENTAGES[i], simulation_percentages[i]))
+
+
+def loop():
     """ Keeps track of all the roll results"""
     results = {2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0}
     for i in range(1000):  # Repeats the dice roll 1000 times
@@ -20,21 +38,18 @@ def main():
         for x in range(13):
             if x == value:  # Checks if result equals a given key
                 results[x] = results[x] + 1
-    output(results)
+    output(percentage(results))
 
 
-def output(totals):
-    """Outputs the percentages and expected percentages"""
-    expected_percentages = {2: 2.78, 3: 5.56, 4: 8.33, 5: 11.11, 6: 13.89, 7: 16.67, 8: 13.89, 9: 11.11, 10: 8.33, 11: 5.56, 12: 2.78}
-    percentages = {}
-
-    # Convert each total into percentages of all rolls
-    for i in range(2, 13):
-        percentages[i] = (float(totals[i])/10)
-
-    # Output results and expected
-    print(percentages)
-    print(expected_percentages)
+def main():
+    while True:
+        choice = str(input("What would you like to do?: \n 1. Roll and Display Table \n 2. Exit \n "))
+        if choice == "1":
+            loop()
+        elif choice == "2":
+            break
+        else:
+            print("Invalid input, enter 1 or 2")
 
 
 main()
